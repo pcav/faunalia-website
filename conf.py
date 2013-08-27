8d38d93f9e5f8368fde30791666c26acee246c31
pcav <cavallini@faunalia.it>
2013-08-27 07:15:19 +0200

    Better name for templates dir

--- conf.py
+++ conf.py
@@ -32,8 +32,7 @@ extensions = ['rst2pdf.pdfbuilder', 'sphinx.ext.todo', 'sphinx.ext.mathjax']
 # default is false
 
 # Add any paths that contain templates here, relative to this directory.
-
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
