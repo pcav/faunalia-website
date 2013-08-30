e643d3357fbe79138166c167066fb12f98f4f2e0
pcav <cavallini@faunalia.it>
2013-08-30 14:27:18 +0200

    Script to publish the website (untested)

--- publish.sh
+++ publish.sh
@@ -1,31 +1,10 @@
 #!/bin/sh
 
-##UNFINISHED, JUST A DRAFT
-
-#optional
-read -r -p "Do you want to update the CVs? [Y/n] " prompt
-#if [[ $response =~ ^([yY][sS]|)$ ]]
-#if [[ $prompt =~ [yY] ]]
-
-#read -r -p "Are you sure? [Y/n]" response
-#response=${response,,}    # tolower
-#if [[ $response =~ ^(yes|y| ) ]]
-
-#then
-#echo "Your reply to CV update: " $response
-#    make cv
-#else
-#fi
-
-while true; do
-    read -p "Do you wish to update the CVs? [Y/n]" yn
-    case $yn in
-#        [Yy]* ) make cv; break;;
-        [Yy]* ) echo "Your reply to CV update: " $yn
-        [Nn]* ) echo "Skipping compilation of CVs"
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+read -p "Do you wish to update the CVs? [Y/n]" choice1
+case "$choice1" in 
+  y|Y ) echo "Updating CVs";make cv;
+  * ) echo "Skipping compilation of CVs";;
+esac
 
 touch *.rst
 #make gettext
@@ -43,12 +22,8 @@ grep -c \"\" translated/*/*.po
 echo "Total untranslated strings"
 grep -l \"\" translated/*/*.po | wc
 
-read -p "Are you sure you want to publish the website anyway? [Y/n] " prompt
-#response=${response,,}    # tolower
-#if [[ $response =~ ^(yes|y)$ ]]
-if [ $prompt =~ [yY] ]
-then
-    make html
-else
-    echo "Messages compiled, site not published"
-fi
+read -p "Are you sure you want to publish the website anyway? [Y/n]" choice2
+case "$choice2" in 
+  y|Y ) echo "Publishing website";make html;
+  * ) echo "Messages compiled, site not published";;
+esac
