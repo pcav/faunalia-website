b08be82023a981ff08d71388f43b85f5ad060b7c
pcav <cavallini@faunalia.it>
2013-09-04 15:52:57 +0200

    Better option for publishing script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -p "Do you wish to update the CVs? [Y/n]" choice1
+read -p "Do you wish to update the CVs? [y/n]" choice1
 case "$choice1" in 
   y|Y ) echo "Updating CVs";make cv;;
   * ) echo "Skipping compilation of CVs";;
@@ -22,7 +22,7 @@ grep -c \"\" translated/*/*.po
 echo "Total potentially untranslated strings"
 grep -l \"\" translated/*/*.po | wc
 
-read -p "Are you sure you want to publish the website anyway? [Y/n]" choice2
+read -p "Are you sure you want to publish the website anyway? [y/n]" choice2
 case "$choice2" in 
   y|Y ) echo "Publishing website";make html;;
   * ) echo "Messages compiled, site not published";;
