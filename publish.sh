26cca3dbfc5e9a088abdd366429bd106c8476b78
pcav <cavallini@faunalia.it>
2013-11-16 16:25:28 +0100

    Cleanup of publishing script

--- publish.sh
+++ publish.sh
@@ -19,19 +19,6 @@ echo "**Statistics of untranslated (u) and fuzzy (f) strings**"
 
 pocount --incomplete --short-strings translated/*/*.po
 
-#echo "Number of unfinished strings"
-#grep -c  fuzzy translated/*/*.po
-#echo ""
-#echo "Verify the above: 1 per file is normal, >1 needs a correction"
-#echo "Total unfinished strings"
-#grep -l fuzzy translated/*/*.po | wc
-#echo "Beware, untransalted strings are not taken into account"
-#echo "in the count above"
-#echo "Number of potentially untranslated strings"
-#grep -c \"\" translated/*/*.po
-#echo "Total potentially untranslated strings"
-#grep -l \"\" translated/*/*.po | wc
-
 read -p "Are you sure you want to publish the website anyway? [y/n]" choice2
 case "$choice2" in 
   y|Y ) echo "Publishing website";make html;;
